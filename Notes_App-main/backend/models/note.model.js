a7f6dbe03da343c58bbb9a5d16272206d297af6a
Sahithiap <162537012+Sahithiap@users.noreply.github.com>
2025-09-04 23:38:13 +0530

    Update note.model.js

--- Notes_App-main/backend/models/note.model.js
+++ Notes_App-main/backend/models/note.model.js
@@ -28,17 +28,12 @@ const noteSchema = new Schema({
     ref: "User",
     required: true,
   },
-});
-const mongoose = require("mongoose");
-
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String },
   screenshot: { type: String }, 
   tags: { type: [String], default: [] },
   isPinned: { type: Boolean, default: false },
-}, { timestamps: true });
-
+}, { 
+  timestamps: true 
+});
 
 
 module.exports = mongoose.model("Note", noteSchema);
