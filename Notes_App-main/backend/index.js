a3e8f2e54089087e9e7995d1d3dd012658a0771c
Sahithiap <162537012+Sahithiap@users.noreply.github.com>
2025-09-04 18:09:46 +0530

    Update index.js

--- Notes_App-main/backend/index.js
+++ Notes_App-main/backend/index.js
@@ -13,7 +13,7 @@ connectDB();
 
 // CORS configuration
 const corsOptions = {
-  origin: ['https://notes-app-lvja.vercel.app', 'http://localhost:5173'],
+  origin: ['https://notes-app-wcr7.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 };
