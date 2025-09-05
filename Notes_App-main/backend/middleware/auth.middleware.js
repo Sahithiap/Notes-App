21d75a678a29bd9cea1dd11049728f3c91a78f80
Sahithiap <162537012+Sahithiap@users.noreply.github.com>
2025-09-05 15:39:38 +0530

    Update auth.middleware.js

--- Notes_App-main/backend/middleware/auth.middleware.js
+++ Notes_App-main/backend/middleware/auth.middleware.js
@@ -1,16 +1,20 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const Token = require("../models/token.model");
 
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1];
+  if (!token) return res.sendStatus(401);
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers["authorization"]
-    const token = authHeader && authHeader.split(" ")[1]
-    
-    if(!token) return res.sendStatus(401)
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(401)
-        req.user = user
-        next()
-    })
-}
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
+    if (err) return res.sendStatus(401);
 
-module.exports = {authenticateToken};
+    const foundToken = await Token.findOne({ token });
+    if (!foundToken) return res.sendStatus(401);
+
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = { authenticateToken };
