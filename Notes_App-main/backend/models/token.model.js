5db4935e9d64e53e5e2925890722cbf6fb87c1db
Sahithiap <162537012+Sahithiap@users.noreply.github.com>
2025-09-05 15:33:01 +0530

    Create token.model.js

---
+++ Notes_App-main/backend/models/token.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  token: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600 // token expires from DB after 1 hour automatically
+  }
+});
+
+module.exports = mongoose.model("Token", tokenSchema);
